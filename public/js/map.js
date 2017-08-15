9e4a928a951be05cc89b066d7e0869468fece260
Jing Jia <nycjing@gmail.com>
2017-08-15 18:13:37 -0400

    place marker

--- public/js/map.js
+++ public/js/map.js
@@ -1,3 +1,5 @@
+var currentMap;
+
 $(function initializeMap (){
 
   var fullstackAcademy = new google.maps.LatLng(40.705086, -74.009151);
@@ -33,28 +35,12 @@ $(function initializeMap (){
 
   var mapCanvas = document.getElementById('map-canvas');
 
-  var currentMap = new google.maps.Map(mapCanvas, {
+  currentMap = new google.maps.Map(mapCanvas, {
     center: fullstackAcademy,
     zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     styles: styleArr
   });
-
-  var iconURLs = {
-    hotel: '/images/lodging_0star.png',
-    restaurant: '/images/restaurant.png',
-    activity: '/images/star-3.png'
-  };
-
-  function drawMarker (type, coords) {
-    var latLng = new google.maps.LatLng(coords[0], coords[1]);
-    var iconURL = iconURLs[type];
-    var marker = new google.maps.Marker({
-      icon: iconURL,
-      position: latLng
-    });
-    marker.setMap(currentMap);
-  }
 
   //drawMarker('hotel', [40.705137, -74.007624]);
   //drawMarker('restaurant', [40.705137, -74.013940]);
@@ -62,6 +48,20 @@ $(function initializeMap (){
 
 });
 
-module.exports = {
-  drawMarker: drawMarker
-}
+
+function drawMarker (type, coords) {
+    var iconURLs = {
+        hotel: '/images/lodging_0star.png',
+        restaurant: '/images/restaurant.png',
+        activity: '/images/star-3.png'
+    };
+
+    var latLng = new google.maps.LatLng(coords[0], coords[1]);
+    var iconURL = iconURLs[type];
+    var marker = new google.maps.Marker({
+        icon: iconURL,
+        position: latLng
+    });
+    marker.setMap(currentMap);
+};
+
